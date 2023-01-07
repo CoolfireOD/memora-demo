2b4c53fc8f5d198821ea2b72d5a75e08b8685ced
CoolfireOD <mishazinenkofighter@gmail.com>
2023-01-08 01:29:35 +0200

    feat: improve card and error page

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,17 +1,67 @@
-import { Box, Alert } from "@mui/material";
+import { Box, Typography, Link } from "@mui/material";
 import React from "react";
-import { useRouteError } from "react-router-dom";
-import { UNEXPECTED_ERROR_MESSAGE } from "../const";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError() as unknown;
   console.error(error);
 
+  const navigate = useNavigate();
+
   return (
-    <Box>
-      <Alert severity="error" variant="filled">
-        <strong>{UNEXPECTED_ERROR_MESSAGE}</strong>
-      </Alert>
+    <Box
+      sx={{
+        position: "fixed",
+        width: "100%",
+        height: "100%",
+        bgcolor: "white",
+        top: 0,
+        left: 0,
+        zIndex: 9999,
+        display: "flex",
+        flexDirection: "column",
+        rowGap: 2,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h3">There&apos;s been a glitch...</Typography>
+      <Typography
+        variant="h4"
+        sx={{
+          maxWidth: "800px",
+        }}
+      >
+        We&apos;re not quite sure what went wrong. You can go back or try some
+        of this helpful links:{" "}
+      </Typography>
+      <Typography
+        variant="h4"
+        sx={{ display: "flex", columnGap: 4, cursor: "pointer" }}
+      >
+        {" "}
+        <Link
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Home
+        </Link>{" "}
+        <Link
+          onClick={() => {
+            navigate("/profile");
+          }}
+        >
+          Profile
+        </Link>{" "}
+        <Link
+          onClick={() => {
+            navigate("/notifications");
+          }}
+        >
+          News
+        </Link>
+      </Typography>
     </Box>
   );
 }
--- src/routes/cardRoute/CardRoute.tsx
+++ src/routes/cardRoute/CardRoute.tsx
@@ -1,40 +1,87 @@
 import React, { FC } from "react";
-import { Box } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import { useIdParam } from "../../hooks/useIdParam";
 import { CARDS_QUERY_KEY } from "../recordsRoute/const";
 import * as API from "./api";
 import { useQuery } from "@tanstack/react-query";
-import { CardInfoContent } from "./components/CardInfoContent";
 import { CardInfoHeader } from "./components/CardInfoHeader";
 
 export const CardRoute: FC = () => {
-    const cardId = useIdParam();
+  const cardId = useIdParam();
 
-    const { data: cardItems } = useQuery(CARDS_QUERY_KEY, API.getCards);
+  const { data: cardItems } = useQuery(CARDS_QUERY_KEY, API.getCards);
 
-    const cardData = cardItems?.filter((card) => card.id === cardId);
+  const cardData = cardItems?.filter((card) => card.id === cardId);
 
-    return (
-        <Box
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        rowGap: 2,
+        minWidth: "50%",
+      }}
+    >
+      {cardData && (
+        <>
+          <CardInfoHeader />
+          <Typography
+            variant="h4"
             sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                rowGap: 4,
-                minWidth: "50%",
-                alignSelf: "center",
+              fontStyle: "bold",
+              fontWeight: 700,
+              letterSpacing: 1,
+              textIndent: "1rem",
+              alignSelf: "flex-start",
             }}
-        >
-            {cardData && (
-                <>
-                    <CardInfoHeader />
-                    <CardInfoContent
-                        title={cardData[0].title}
-                        subtitle={cardData[0].subtitle}
-                        description={cardData[0].description}
-                    />
-                </>
-            )}
-        </Box>
-    );
+          >
+            Main Info
+          </Typography>
+          <Paper
+            elevation={3}
+            sx={{
+              p: 6,
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              rowGap: 2,
+            }}
+          >
+            <Box>
+              <Typography variant="h4" sx={{ fontWeight: 700 }}>
+                {cardData[0].title}
+              </Typography>
+              <Typography variant="h6" sx={{ textIndent: "2rem" }}>
+                {cardData[0].subtitle}
+              </Typography>
+            </Box>
+            <Typography variant="h5">{cardData[0].description}</Typography>
+          </Paper>
+          <Typography
+            variant="h4"
+            sx={{
+              fontStyle: "bold",
+              fontWeight: 700,
+              letterSpacing: 1,
+              textIndent: "1rem",
+              alignSelf: "flex-start",
+            }}
+          >
+            Attached files
+          </Typography>
+          <Paper
+            elevation={3}
+            sx={{
+              p: 6,
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              rowGap: 2,
+            }}
+          ></Paper>
+        </>
+      )}
+    </Box>
+  );
 };
